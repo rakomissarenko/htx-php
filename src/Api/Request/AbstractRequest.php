29445cf9b0f4ecdf8a3119f85f311ad67e62d2e9
Roman <r.komissarenko@unitpay.ru>
2023-09-15 19:37:57 +0300

    Add AbstractRequest

---
+++ src/Api/Request/AbstractRequest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Feralonso\Htx\Api\Request;
+
+use Feralonso\Htx\Exceptions\HtxValidateException;
+
+abstract class AbstractRequest implements RequestInterface
+{
+    protected const FIELD_CLIENT_ORDER_ID = 'clientOrderId';
+    protected const FIELD_CURRENCY = 'currency';
+    protected const FIELD_TS = 'ts';
+
+    protected const METHOD_GET = 'GET';
+    private const METHOD_POST = 'POST';
+
+    private const PERMISSION_READ = 'Read';
+    protected const PERMISSION_TRADE = 'Trade';
+    protected const PERMISSION_WITHDRAW = 'Withdraw';
+
+    protected const METHOD = self::METHOD_POST;
+    protected const PATH = '';
+    protected const PERMISSION = self::PERMISSION_READ;
+
+    public function getMethod(): string
+    {
+        return static::METHOD;
+    }
+
+    public function isMethodGet(): bool
+    {
+        return static::METHOD === self::METHOD_GET;
+    }
+
+    public function getPath(): string
+    {
+        return static::PATH;
+    }
+
+    public function getPermission(): string
+    {
+        return static::PERMISSION;
+    }
+
+    public function validate(): void
+    {
+    }
+
+    /**
+     * @throws HtxValidateException
+     */
+    protected function validateList(string $value, string $name, array $list): void
+    {
+        if (!in_array($value, $list, true)) {
+            $this->throwValidateException($name);
+        }
+    }
+
+    /**
+     * @throws HtxValidateException
+     */
+    protected function validateInteger(string $value, string $name): void
+    {
+        if (preg_match('/\D+/', $value)) {
+            $this->throwValidateException($name);
+        }
+    }
+
+    /**
+     * @throws HtxValidateException
+     */
+    protected function validateNumeric(string $value, string $name): void
+    {
+        if (!is_numeric($value)) {
+            $this->throwValidateException($name);
+        }
+    }
+
+    /**
+     * @throws HtxValidateException
+     */
+    protected function validateRange(string $value, string $name, string $min, ?string $max = null): void
+    {
+        if ($value < $min || ($max && $value > $max)) {
+            $this->throwValidateException($name);
+        }
+    }
+
+    /**
+     * @throws HtxValidateException
+     */
+    protected function validateSize(string $value, string $name, int $size): void
+    {
+        if (mb_strlen($value) > $size) {
+            $this->throwValidateException($name);
+        }
+    }
+
+    /**
+     * @throws HtxValidateException
+     */
+    protected function throwValidateException(string $name): void
+    {
+        throw new HtxValidateException(
+            HtxValidateException::getErrorMessage($name),
+            HtxValidateException::ERROR_INPUT_VALUE_INVALID
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [];
+    }
+}
