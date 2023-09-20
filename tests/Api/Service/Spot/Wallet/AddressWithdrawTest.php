883612bd6f1954165da9e80b860adbf50f9b00df
Roman <r.komissarenko@unitpay.ru>
2023-09-20 14:33:39 +0300

    Add ValueHelper in tests

--- tests/Api/Service/Spot/Wallet/AddressWithdrawTest.php
+++ tests/Api/Service/Spot/Wallet/AddressWithdrawTest.php
@@ -4,18 +4,48 @@ namespace Feralonso\Tests\Api\Service\Spot\Wallet;
 
 use Feralonso\Htx\Api\Request\Spot\Wallet\AddressWithdrawRequest;
 use Feralonso\Htx\Exceptions\HtxValidateException;
+use Feralonso\Tests\Helper\ValueHelper;
 use PHPUnit\Framework\TestCase;
 
 class AddressWithdrawTest extends TestCase
 {
     /**
      * @throws HtxValidateException
+     *
+     * @dataProvider validateProvider
      */
-    public function testValidate(): void
+    public function testValidate(string $currency): void
     {
         $this->expectNotToPerformAssertions();
 
-        $request = new AddressWithdrawRequest('usdt');
+        $request = new AddressWithdrawRequest($currency);
         $request->validate();
+    }
+
+    /**
+     * @throws HtxValidateException
+     *
+     * @dataProvider invalidateProvider
+     */
+    public function testInvalidate(string $currency): void
+    {
+        $this->expectException(HtxValidateException::class);
+
+        $request = new AddressWithdrawRequest($currency);
+        $request->validate();
+    }
+
+    public function validateProvider(): array
+    {
+        return [
+            [ValueHelper::CURRENCY],
+        ];
+    }
+
+    public function invalidateProvider(): array
+    {
+        return [
+            [ValueHelper::EMPTY_STRING],
+        ];
     }
 }
---
+++ tests/Helper/ValueHelper.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Feralonso\Tests\Helper;
+
+class ValueHelper
+{
+    public const EMPTY_STRING = '';
+    public const CURRENCY = 'usdt';
+}
