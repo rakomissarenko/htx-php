7a0e9884d81278bc176ecbe97018cb660b63bfd0
Roman <r.komissarenko@unitpay.ru>
2023-09-22 19:17:40 +0300

    Add FieldResponseHelper

---
+++ src/Api/Helper/FieldResponseHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Feralonso\Htx\Api\Helper;
+
+class FieldResponseHelper
+{
+    public const FIELD_ASK = 'ask';
+    public const FIELD_ASK_SIZE = 'askSize';
+    public const FIELD_BID = 'bid';
+    public const FIELD_BID_SIZE = 'bidSize';
+    public const FIELD_CLOSE = 'close';
+    public const FIELD_COUNT = 'count';
+    public const FIELD_HIGH = 'high';
+    public const FIELD_LOW = 'low';
+    public const FIELD_OPEN = 'open';
+    public const FIELD_VOL = 'vol';
+}
