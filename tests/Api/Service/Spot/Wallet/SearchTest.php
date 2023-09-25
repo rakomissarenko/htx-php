cb6c2c5ccd96da76abc9a5021c915ae300cde75f
Roman <r.komissarenko@unitpay.ru>
2023-09-25 17:56:32 +0300

    Use SearchResponse in test

--- tests/Api/Service/Spot/Wallet/SearchTest.php
+++ tests/Api/Service/Spot/Wallet/SearchTest.php
@@ -4,11 +4,18 @@ namespace Feralonso\Tests\Api\Service\Spot\Wallet;
 
 use Feralonso\Htx\Api\Helper\EnumHelper;
 use Feralonso\Htx\Api\Request\Spot\Wallet\SearchRequest;
+use Feralonso\Htx\Api\Response\Spot\Wallet\SearchResponse;
 use Feralonso\Htx\Exceptions\HtxValidateException;
+use Feralonso\Tests\Helper\FileHelper;
 use PHPUnit\Framework\TestCase;
 
 class SearchTest extends TestCase
 {
+    private const RESPONSES_SUCCESS = [
+        'Spot/Wallet/SearchDepositSuccess.json',
+        'Spot/Wallet/SearchWithdrawSuccess.json',
+    ];
+
     /**
      * @throws HtxValidateException
      *
@@ -21,6 +28,18 @@ class SearchTest extends TestCase
         $this->getRequest($type)->validate();
     }
 
+    /**
+     * @throws HtxValidateException
+     *
+     * @dataProvider responseSuccessProvider
+     */
+    public function testResponseSuccess(string $content): void
+    {
+        $response = $this->getResponse($content);
+
+        $this->assertArrayHasKey('data', $response->toArray());
+    }
+
     public function validateProvider(): array
     {
         return [
@@ -28,8 +47,21 @@ class SearchTest extends TestCase
         ];
     }
 
+    public function responseSuccessProvider(): array
+    {
+        return FileHelper::getResponsesProvider(self::RESPONSES_SUCCESS);
+    }
+
     private function getRequest(string $type): SearchRequest
     {
         return new SearchRequest($type);
+    }
+
+    /**
+     * @throws HtxValidateException
+     */
+    private function getResponse(string $response): SearchResponse
+    {
+        return new SearchResponse($response);
     }
 }
