0ef9464eaa77e83fb74f9f2b1b643af8f0f61fb2
Roman <r.komissarenko@unitpay.ru>
2023-09-25 15:43:02 +0300

    Add AddressDepositResponse

---
+++ src/Api/Response/Spot/Wallet/AddressDepositResponse.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Feralonso\Htx\Api\Response\Spot\Wallet;
+
+use Feralonso\Htx\Api\Data\AddressData;
+use Feralonso\Htx\Api\Data\TickerData;
+use Feralonso\Htx\Api\Helper\FormatHelper;
+use Feralonso\Htx\Api\Response\AbstractResponse;
+
+class AddressDepositResponse extends AbstractResponse
+{
+    /**
+     * @var AddressData[]
+     */
+    private array $addresses = [];
+
+    public function __construct(string $response)
+    {
+        parent::__construct($response);
+
+        $responseArray = $this->toArray();
+
+        $data = FormatHelper::getArrayValueInArray($responseArray, self::FIELD_DATA);
+        foreach ($data as $addressData) {
+            if (is_array($addressData)) {
+                $this->addresses[] = TickerData::initByArray($addressData);
+            }
+        }
+    }
+
+    public function getAddresses(): array
+    {
+        return $this->addresses;
+    }
+}
