e735dcd970d1170fed72abbf0f3ec8ba5eb1976d
Roman <r.komissarenko@unitpay.ru>
2023-09-26 13:33:00 +0300

    Add validateRequest method in AbstractTest

--- tests/Api/Service/AbstractTest.php
+++ tests/Api/Service/AbstractTest.php
@@ -2,12 +2,24 @@
 
 namespace Feralonso\Tests\Api\Service;
 
+use Feralonso\Htx\Api\Request\RequestInterface;
+use Feralonso\Htx\Exceptions\HtxValidateException;
 use Feralonso\Tests\Helper\FileHelper;
 use PHPUnit\Framework\TestCase;
 
 abstract class AbstractTest extends TestCase
 {
     protected const RESPONSES_SUCCESS = [];
+
+    /**
+     * @throws HtxValidateException
+     */
+    protected function validateRequest(RequestInterface $request): void
+    {
+        $this->expectNotToPerformAssertions();
+
+        $request->validate();
+    }
 
     public function responseSuccessProvider(): array
     {
