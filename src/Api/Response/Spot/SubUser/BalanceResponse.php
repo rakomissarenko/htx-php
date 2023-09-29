342cd41d6178757e2bb605205a0717a71d591fed
Roman <r.komissarenko@unitpay.ru>
2023-09-29 15:01:04 +0300

    Use BalanceAccountData in BalanceResponse SubUser

--- src/Api/Response/Spot/SubUser/BalanceResponse.php
+++ src/Api/Response/Spot/SubUser/BalanceResponse.php
@@ -2,8 +2,36 @@
 
 namespace Feralonso\Htx\Api\Response\Spot\SubUser;
 
+use Feralonso\Htx\Api\Data\BalanceAccountData;
+use Feralonso\Htx\Api\Helper\FormatHelper;
 use Feralonso\Htx\Api\Response\AbstractResponse;
 
 class BalanceResponse extends AbstractResponse
 {
+    /**
+     * @var BalanceAccountData[]
+     */
+    private array $balanceAccountsData = [];
+
+    public function __construct(string $response)
+    {
+        parent::__construct($response);
+
+        $responseArray = $this->toArray();
+
+        $data = FormatHelper::getArrayValueInArray($responseArray, self::FIELD_DATA);
+        if ($data) {
+            foreach ($data as $balanceAccountData) {
+                $this->balanceAccountsData[] = BalanceAccountData::initByArray($balanceAccountData);
+            }
+        }
+    }
+
+    /**
+     * @return BalanceAccountData[]
+     */
+    public function getBalanceAccountsData(): array
+    {
+        return $this->balanceAccountsData;
+    }
 }
