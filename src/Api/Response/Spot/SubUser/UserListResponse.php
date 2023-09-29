be1b202e1e4d219704d76de4fd0ddedc60ad4479
Roman <r.komissarenko@unitpay.ru>
2023-09-29 21:49:47 +0300

    Use SubUserData in UserListResponse

--- src/Api/Response/Spot/SubUser/UserListResponse.php
+++ src/Api/Response/Spot/SubUser/UserListResponse.php
@@ -2,8 +2,33 @@
 
 namespace Feralonso\Htx\Api\Response\Spot\SubUser;
 
+use Feralonso\Htx\Api\Data\SubUserData;
+use Feralonso\Htx\Api\Helper\FormatHelper;
 use Feralonso\Htx\Api\Response\AbstractResponse;
 
 class UserListResponse extends AbstractResponse
 {
+    /**
+     * @var SubUserData[]
+     */
+    private array $subUsers = [];
+
+    public function __construct(string $response)
+    {
+        parent::__construct($response);
+
+        $responseArray = $this->toArray();
+
+        $data = FormatHelper::getArrayValueInArray($responseArray, self::FIELD_DATA);
+        if ($data) {
+            foreach ($data as $subUserData) {
+                $this->subUsers[] = SubUserData::initByArray($subUserData);
+            }
+        }
+    }
+
+    public function getSubUsers(): array
+    {
+        return $this->subUsers;
+    }
 }
