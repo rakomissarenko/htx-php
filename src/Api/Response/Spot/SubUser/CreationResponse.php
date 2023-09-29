1893b40310d46de8f34b677bcfc34d3ed7a3ffcd
Roman <r.komissarenko@unitpay.ru>
2023-09-29 21:28:09 +0300

    Use UserData in CreationResponse

--- src/Api/Response/Spot/SubUser/CreationResponse.php
+++ src/Api/Response/Spot/SubUser/CreationResponse.php
@@ -2,8 +2,36 @@
 
 namespace Feralonso\Htx\Api\Response\Spot\SubUser;
 
+use Feralonso\Htx\Api\Data\UserData;
+use Feralonso\Htx\Api\Helper\FormatHelper;
 use Feralonso\Htx\Api\Response\AbstractResponse;
 
 class CreationResponse extends AbstractResponse
 {
+    /**
+     * @var UserData[]
+     */
+    private array $users = [];
+
+    public function __construct(string $response)
+    {
+        parent::__construct($response);
+
+        $responseArray = $this->toArray();
+
+        $data = FormatHelper::getArrayValueInArray($responseArray, self::FIELD_DATA);
+        if ($data) {
+            foreach ($data as $userData) {
+                $this->users[] = UserData::initByArray($userData);
+            }
+        }
+    }
+
+    /**
+     * @return UserData[]
+     */
+    public function getUsers(): array
+    {
+        return $this->users;
+    }
 }
