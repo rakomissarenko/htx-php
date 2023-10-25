23bece5c32f77cb1a51af7d15840c8d1a87035cc
Roman <r.komissarenko@unitpay.ru>
2023-10-25 12:18:14 +0300

    Add field nextId in AddressWithdrawResponse

--- src/Api/Response/Spot/Wallet/AddressWithdrawResponse.php
+++ src/Api/Response/Spot/Wallet/AddressWithdrawResponse.php
@@ -2,5 +2,24 @@
 
 namespace Feralonso\Htx\Api\Response\Spot\Wallet;
 
+use Feralonso\Htx\Api\Helper\FieldResponseHelper;
+use Feralonso\Htx\Api\Helper\FormatHelper;
+
 class AddressWithdrawResponse extends AddressDepositResponse
-{}
+{
+    private ?string $nextId;
+
+    public function __construct(string $response)
+    {
+        parent::__construct($response);
+
+        $responseArray = $this->toArray();
+
+        $this->nextId = FormatHelper::getNumericValueInArray($responseArray, FieldResponseHelper::FIELD_NEXT_ID);
+    }
+
+    public function getNextId(): ?string
+    {
+        return $this->nextId;
+    }
+}
