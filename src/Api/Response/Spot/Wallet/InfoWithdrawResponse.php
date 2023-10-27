f7e891bb5d251370a748b7cc3c1fe149c1d2a7a4
Roman <r.komissarenko@unitpay.ru>
2023-10-27 11:36:37 +0300

    Add methods in InfoWithdrawResponse

--- src/Api/Response/Spot/Wallet/InfoWithdrawResponse.php
+++ src/Api/Response/Spot/Wallet/InfoWithdrawResponse.php
@@ -2,8 +2,28 @@
 
 namespace Feralonso\Htx\Api\Response\Spot\Wallet;
 
+use Feralonso\Htx\Api\Data\TransactionData;
+use Feralonso\Htx\Api\Helper\FormatHelper;
 use Feralonso\Htx\Api\Response\AbstractResponse;
 
 class InfoWithdrawResponse extends AbstractResponse
 {
+    private ?TransactionData $transactionData = null;
+
+    public function __construct(string $response)
+    {
+        parent::__construct($response);
+
+        $responseArray = $this->toArray();
+
+        $data = FormatHelper::getArrayValueInArray($responseArray, self::FIELD_DATA);
+        if ($data) {
+            $this->transactionData = TransactionData::initByArray($data);
+        }
+    }
+
+    public function getTransactionData(): ?TransactionData
+    {
+        return $this->transactionData;
+    }
 }
